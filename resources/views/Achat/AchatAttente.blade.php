402099d2890259f07ce95c1158fb952d17d0b617
LasnamiWalid <lasnami.walid@outlook.com>
2020-11-18 14:28:08 +0100

    Achat table search

--- resources/views/Achat/AchatAttente.blade.php
+++ resources/views/Achat/AchatAttente.blade.php
@@ -36,7 +36,7 @@
    
 
     <div>
-        <table class="table table-striped table-dark">
+        <table class="table table-striped table-dark"  id="table_id" class="display">
           <thead>
             <tr>
               <th scope="col"><B>N° FP</B></th>
